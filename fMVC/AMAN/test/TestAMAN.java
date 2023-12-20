29615e58f271e8bfd427cd9a6ead3f87a9fa3f2b
a-bombarda <andrea.bombarda@unibg.it>
2023-12-20 12:05:05 +0100

    Scenarios and tests

--- fMVC/AMAN/test/TestAMAN.java
+++ fMVC/AMAN/test/TestAMAN.java
@@ -10,7 +10,7 @@ import view.AMANView;
 public class TestAMAN {
 
 	@Test
-	public void testAMAN() throws Exception {
+	public void testAMAN_MoveDown() throws Exception {
 		// Define the model
 		AsmetaFMVCModel model = new AsmetaFMVCModel("model/aman2.asm");
 		AMANView view = new AMANView();
@@ -18,8 +18,33 @@ public class TestAMAN {
 		controller.updateAndSimulate(null);
 		view.setVisible(true);
 		// Create the test runner
-		AsmetaFMVCTestRunner runner = new AsmetaFMVCTestRunner(view, controller, "model/scenario3.avalla", Arrays.asList("NONE"));
+		AsmetaFMVCTestRunner runner = new AsmetaFMVCTestRunner(view, "model/ScenarioMoveDown.avalla", Arrays.asList("NONE"), 500);
 		runner.runTest();
 	}
 	
+	@Test
+	public void testAMAN_MoveUP() throws Exception {
+		// Define the model
+		AsmetaFMVCModel model = new AsmetaFMVCModel("model/aman2.asm");
+		AMANView view = new AMANView();
+		AMANController controller = new AMANController(model, view);
+		controller.updateAndSimulate(null);
+		view.setVisible(true);
+		// Create the test runner
+		AsmetaFMVCTestRunner runner = new AsmetaFMVCTestRunner(view, "model/ScenarioMoveUP.avalla", Arrays.asList("NONE"), 500);
+		runner.runTest();
+	}
+	
+	@Test
+	public void testAMAN_HOLD() throws Exception {
+		// Define the model
+		AsmetaFMVCModel model = new AsmetaFMVCModel("model/aman2.asm");
+		AMANView view = new AMANView();
+		AMANController controller = new AMANController(model, view);
+		controller.updateAndSimulate(null);
+		view.setVisible(true);
+		// Create the test runner
+		AsmetaFMVCTestRunner runner = new AsmetaFMVCTestRunner(view, "model/ScenarioHOLD.avalla", Arrays.asList("NONE"), 500);
+		runner.runTest();
+	}
 }
